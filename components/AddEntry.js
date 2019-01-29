5c9bd9b8d68a6d3d4a152acf76b13cded03e9a7f
Tom Workman <tworkman512@gmail.com>
2019-01-29 13:54:09 -0600

    adds methods to increment, decrement, and slide

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,37 @@ export default class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const count = state[metric] + step
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      }
+    })
+  }
+
+  decrement = (metric) => {
+    this.setState((state) => {
+      const count = state[metric] - getMetricMetaInfo(metric).step
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count,
+      }
+    })
+  }
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+        [metric]: value,
+    }))
+  }
+
   render() {
     return (
       <View>
