91f6e613c7fc7515e0827274cec82008761ef765
Tom Workman <tworkman512@gmail.com>
2019-01-29 15:23:30 -0600

    Adds stepper button with rn TouchableOpacity

--- components/EntryStepper.js
+++ components/EntryStepper.js
@@ -2,12 +2,25 @@ import React from 'react'
 import {
   View,
   Text,
+  TouchableOpacity,
 } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default function EntryStepper() {
+export default function EntryStepper({ max, unit, step, value, onIncrement, onDecrement }) {
   return (
     <View>
-      <Text>EntryStepper</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name='minus' size={30} color={'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name='plus' size={30} color={'black'} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
