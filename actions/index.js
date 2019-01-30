31da9bc8612539b10bc1f2c37ee366957ff00723
Tom Workman <tworkman512@gmail.com>
2019-01-30 11:24:32 -0600

    Add initial Redux actions

---
+++ actions/index.js
@@ -0,0 +1,14 @@
+export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES'
+export const ADD_ENTRY = 'ADD_ENTRY'
+
+export function receiveEntries(entries) {
+  return {
+    type: RECEIVE_ENTRIES,
+    entries,
+  }
+}
+
+export function addEntry(entry) {
+  type: ADD_ENTRY,
+  entry
+}
