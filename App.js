52d1206636057f64275e15546bc19a4321f3c0ae
Tom Workman <tworkman512@gmail.com>
2019-02-04 12:03:56 -0600

    Adds react native status bar and styles

--- App.js
+++ App.js
@@ -3,19 +3,29 @@ import {
   StyleSheet,
   View,
   Platform,
+  StatusBar,
 } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-
+import { purple, white } from './utils/colors'
+import { Constants } from 'expo'
 import Tabs from './components/Tabs'
+
+function FitnessStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={ createStore(reducer) }>
       <View style={{flex: 1}}>
-        <View style={{height: 20}} />
+        <FitnessStatusBar backgroundColor={purple} barStyle='light-content' />
         <Tabs />
       </View>
       </Provider>
